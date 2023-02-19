2b337988865c2a166d799fd907c46d25e6bbb07d
qingqingsun <sun.qingq@northeastern.edu>
2023-02-19 14:52:58 -0800

    finish challange p2, add pictures for p1

--- public/tuiter/PostSummaryList/Post.js
+++ public/tuiter/PostSummaryList/Post.js
@@ -3,7 +3,7 @@ export default [
     topic: 'Web Development',
     userName: 'ReactJS',
     time: '2h',
-    image: '../../images/react-white.png',
+    image: '../../images/react_white.png',
     title: 'React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs',
   },
   {

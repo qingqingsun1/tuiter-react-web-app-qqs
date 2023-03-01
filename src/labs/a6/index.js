c82c9f0514d425ba10f2a18420d89c797e9f8d2e
qingqingsun <sun.qingq@northeastern.edu>
2023-03-01 15:33:21 -0800

    todo list

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,7 +1,11 @@
 // import {Link}
 //   from "react-router-dom";
-import Nav from "../../nav";
-
+// import Nav from "../../nav";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput
+  from "./conditional-output";
+import TodoItem from "./todo/todo-item";
 
 function Assignment6() {
   return (
@@ -9,8 +13,12 @@ function Assignment6() {
         {/*<Link to="/">Lab</Link> |*/}
         {/*<Link to="/hello">Hello</Link> |*/}
         {/*<Link to="/tuiter">Tuiter</Link>*/}
-        <Nav/>
+        {/*<Nav/>*/}
         <h1>Assignment 6</h1>
+        <TodoItem/>
+        <ConditionalOutput/>
+        <Styles/>
+        <Classes/>
       </div>
   );
 }

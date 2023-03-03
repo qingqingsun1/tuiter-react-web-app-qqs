c79ddf37825aa2d01f0937aa2ab407111922effa
qingqingsun <sun.qingq@northeastern.edu>
2023-03-02 18:25:22 -0800

    finish all

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,8 +1,13 @@
 import Assignment6 from "./a6";
+import {Link}
+  from "react-router-dom";
 
 function Labs() {
   return(
       <div>
+        <Link to="/">Lab</Link> |
+        <Link to="/hello">Hello</Link> |
+        <Link to="/tuiter">Tuiter</Link>
         <Assignment6/>
       </div>
   );
